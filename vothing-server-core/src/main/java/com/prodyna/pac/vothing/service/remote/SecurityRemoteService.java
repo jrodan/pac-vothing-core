ef519f58e167a5d477418088c94354fd61eddd9b
Jan Rodan <jan.rodan@prodyna.com>
2016-05-18 23:13:42 +0200

    fixed web context problem fixed CDI injection problem for remote services open: fix problem with updating a survey

--- vothing-server-core/src/main/java/com/prodyna/pac/vothing/service/remote/SecurityRemoteService.java
+++ vothing-server-core/src/main/java/com/prodyna/pac/vothing/service/remote/SecurityRemoteService.java
@@ -3,26 +3,19 @@ package com.prodyna.pac.vothing.service.remote;
 import com.prodyna.pac.vothing.monitoring.VothingMonitoring;
 import com.prodyna.pac.vothing.security.LoginCredentials;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.ext.Provider;
-
 /**
  * Created by jrodan on 17/05/16.
  */
-@Provider
-@Path("/vothing/security")
-@Consumes(MediaType.APPLICATION_JSON)
-@Produces(MediaType.APPLICATION_JSON)
+//@Provider
+//@Path("/vothing/security")
+//@Consumes(MediaType.APPLICATION_JSON)
+//@Produces(MediaType.APPLICATION_JSON)
 @VothingMonitoring
 public interface SecurityRemoteService {
 
-    @POST
-    @Path("/login")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
+//    @POST
+//    @Path("/login")
+//    @Consumes(MediaType.APPLICATION_JSON)
+//    @Produces(MediaType.APPLICATION_JSON)
     String login(LoginCredentials loginCredentials);
 }
--- vothing-server-core/src/main/java/com/prodyna/pac/vothing/service/remote/SecurityRemoteServiceHelper.java
+++ vothing-server-core/src/main/java/com/prodyna/pac/vothing/service/remote/SecurityRemoteServiceHelper.java
@@ -1,26 +1,37 @@
 package com.prodyna.pac.vothing.service.remote;
 
 import com.prodyna.pac.vothing.Vothing;
+import com.prodyna.pac.vothing.monitoring.VothingMonitoring;
 import com.prodyna.pac.vothing.security.LoginCredentials;
+import com.prodyna.pac.vothing.service.SecurityService;
 import org.slf4j.Logger;
 
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.ext.Provider;
 import java.io.IOException;
 
 import static com.prodyna.pac.vothing.constants.VothingConstants.HTTP_CLIENT_STATUS_LOGIN_INVALID;
 
-//@Provider
-//@Path("/vothing/security")
-//@Consumes(MediaType.APPLICATION_JSON)
-//@Produces(MediaType.APPLICATION_JSON)
-//@VothingMonitoring
+@Provider
+@Path("/vothing/security")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+@VothingMonitoring
 public class SecurityRemoteServiceHelper implements SecurityRemoteService {
 
     @Inject
     private Vothing vothing;
+
+    @Inject
+    private SecurityService securityService;
 
     @Context
     private HttpServletRequest httpRequest;
@@ -31,13 +42,13 @@ public class SecurityRemoteServiceHelper implements SecurityRemoteService {
     @Inject
     private Logger logger;
 
-//    @POST
-//    @Path("/login")
-//    @Consumes(MediaType.APPLICATION_JSON)
-//    @Produces(MediaType.APPLICATION_JSON)
+    @POST
+    @Path("/login")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
     @Override
     public String login(LoginCredentials loginCredentials) {
-        String token = vothing.getSecurityService().login(loginCredentials);
+        String token = securityService.login(loginCredentials);
 
         if (token == null) {
             try {
